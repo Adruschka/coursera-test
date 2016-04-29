0b802810703cc012120157cf26eab68f3a051306
Ada K <Ada K>
2016-04-29 22:47:28 +0200

    Update of module4-solution

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -62,9 +62,8 @@ WARNING!!! WARNING!!!
 (function(){
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i=0; i<names.length; i++) {
-var firstLetter = names[i].charAt(0);
-names[i].toLowerCase();
-  if (firstLetter=="J") {
+var firstLetter = names[i].charAt(0).toLowerCase();
+  if (firstLetter=="j") {
      byeSpeaker.speak(names[i]);
   } else {
      helloSpeaker.speak(names[i]);
